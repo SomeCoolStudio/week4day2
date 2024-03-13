c8a4cb34fac64e23db4f16533e7f358c790dccef
SomeCoolStudio <47907012+SomeCoolStudio@users.noreply.github.com>
2024-03-13 13:45:37 -0700

    Week 4 Day 2

---
+++ dml_hw.sql
@@ -0,0 +1,67 @@
+DELETE FROM customer
+WHERE customer_id in (
+                    SELECT max (customer_id)
+                    FROM customer
+                    GROUP BY customer_name
+                    HAVING count(*) > 1)
+
+INSERT INTO customer(
+    customer_name,
+    phone,
+    email,
+    age,
+    VIP
+) VALUES (
+'Smith',
+'(972)083-4974',
+'Smithy@CT.com',
+29,
+'true'
+),
+(
+  'Mary',
+  '(635)4635414',
+'MaryLamb@CT.com',
+34,
+'true'
+);
+
+DELETE FROM customer WHERE customer_name='Mary';
+
+INSERT INTO sale(
+    customer_id,
+    amount
+) VALUES (
+    2,
+    400
+);
+
+INSERT INTO concession(
+    sale_id,
+    price,
+    concession_name,
+    concession_desc
+) VALUES (
+    2,
+    120,
+    'Family Candy Bundle',
+    'Delicious candy for everyone!'
+)
+
+INSERT INTO movie(
+    ticket_id,
+    movie_title,
+    movie_genre
+) VALUES (
+    1,
+    'Romantic Cheese',
+    'Romantic Comedy'
+)
+
+INSERT INTO ticket (
+    ticket_id,
+    sale_id
+) VALUES (
+    1, 
+    2
+ );
